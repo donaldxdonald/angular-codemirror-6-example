774280373c03bc2c7a05d1e5d4dcd8c12f5823de
donaldxdonald <mzhlovefama@gmail.com>
2023-04-27 00:52:38 +0800

    chore: add vercel analytics

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic'
 
+import { inject } from '@vercel/analytics/*'
 import { AppModule } from './app/app.module'
+
+inject({
+  mode: 'auto',
+})
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err))
