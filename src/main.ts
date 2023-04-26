ca33794c12a9bdfa8d78b1a9315f685b4470d22d
donaldxdonald <mzhlovefama@gmail.com>
2023-04-27 00:54:49 +0800

    chore: fix path

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic'
 
-import { inject } from '@vercel/analytics/*'
+import { inject } from '@vercel/analytics'
 import { AppModule } from './app/app.module'
 
 inject({
